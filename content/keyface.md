4bd018e236b5c8da72e921b722f3001e73c7ea0e
bearachute <sjourand@gmail.com>
2021-10-07 02:09:21 -0500

    inital pelican blob

---
+++ content/keyface.md
@@ -0,0 +1,8 @@
+Title: My first thing here.
+Date: 2021-10-7 1:17
+Category: Biz
+modified: 2021-10-8
+
+
+Following *my face* is a wonderful thing.
+[skottie o](https://google.com/) is the _coolest_ being on earth.
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'skort'
+SITENAME = 'blagh'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('I have no friends!', 'http://nothinghere.net/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Full Stack Python', 'https://www.fullstackpython.com/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
